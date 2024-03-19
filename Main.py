8f9f3065aa12b1f229e1ddd2570adfa79f6dd391
Skillghost28 <salomonorozcojaramillo@gmail.com>
2024-03-19 02:50:48 -0500

    Cambios antes de DEPLOY
    Se hacen los cambios pre-deploy:
    - Funciones 2
    - ETL y GZIPS completos

--- Main.py
+++ Main.py
@@ -1,29 +1,42 @@
 from fastapi import FastAPI
 import pandas as pd
 from funciones import max_playtime_year
+from funciones import max_player_time_per_genre
 
 app = FastAPI()
 
 # Load the DataFrames when the application starts
-df_steam_games = pd.read_parquet('GZIP/df_steam_games.gzip')
-df_user_revs = pd.read_parquet('GZIP/df_user_revs.gzip')
-user_items = pd.read_parquet('GZIP/user_items.gzip')
-hours_per_year = pd.read_parquet('GZIP/hours_per_year.gzip')
+player_maxtime_genre = pd.read_parquet('GZIP/max_playtime_user_genre.gzip')
+hours_per_year = pd.read_parquet('GZIP/max_hours_per_year.gzip')
+
 
 @app.get("/")
 def home():
     return {"message": "Welcome to the Home Page"}
 
 @app.get("/max_playtime_year/{genre}")
-async def max_play_year(genre: str):
+async def max_play_time_year(genre: str):
     """
     Returns the year with the highest total play time for games of the specified genre.
     Parameters:
         - genre: Genre to filter games.
     Returns:
-        - int: Year with the highest total play time for the specified genre. If the genre is not found, returns None.
+        - int: Year with the highest total play time for the specified genre. If the genre is not found, returns a message.
     """
     # Call the PlayTimeGenre function with the loaded DataFrames
     max_year = max_playtime_year(hours_per_year, genre)
+
+    return f"Release year with most playtime for {genre} : {max_year}"
+
+@app.get("/user_for_genre/{genre}")
+async def  user_by_game_genre(genre: str):
+    """
+    Returns the player with the highest total play time for games of the specified genre.
+    Parameters:
+        - genre: Genre to filter games.
+    Returns:
+        - int: Year with the highest total play time for the specified genre. If the genre is not found, returns a message.
+    """
+    user_max_time = max_player_time_per_genre(hours_per_year, player_maxtime_genre, genre)
     
-    return max_year
+    return user_max_time
--- funciones.py
+++ funciones.py
@@ -2,40 +2,54 @@
 import pandas as pd
 import numpy as np
 
+
 def max_playtime_year(hours_per_year, genre):
     """
-    Returns the year with the highest total play time for games of the specified genre.
-    Parameters:
-        - hours_per_year: DataFrame containing total playtime per year and genre.
-        - genre: Genre to filter games.
-    Returns:
-        - int: Year with the highest total play time for the specified genre. If the genre is not found, returns None.
-    """
-    try:
-        genre = genre.lower()
-        # Initialize an empty list to store unique genres
-        unique_genres = []
+    Returns the year with the highest total playtime for the specified genre.
 
-        # Loop through each entry in the 'Genres' column
-        for genres_entry in hours_per_year['Genres']:
-            # Split the entry by comma and strip any leading or trailing spaces
-            genres_list = [genre.strip() for genre in genres_entry.split(',')]
-    
-            # Add each genre from the entry to the list of unique genres
-            for genre in genres_list:
-                if genre not in unique_genres:
-                    unique_genres.append(genre)
-        # Now unique_genres contains a list of unique genres
-                    
-        print(unique_genres)
-        if genre not in unique_genres:
-            return f"Can't find the genre that you're looking for..."
-        # Filter the DataFrame by the specified genre
-        filtered_data = hours_per_year[hours_per_year['Genres'].str.contains(genre, case=False)]
-        # Find the year with the highest total play time
-        max_playtime_year = filtered_data.loc[filtered_data['Total_Playtime'].idxmax(), 'Release_Year']
-        return int(max_playtime_year)
+    Parameters:
+        genre (str): The genre to search for.
+
+    Returns:
+        int or Str: The year with the highest total playtime for the genre, or a message if the genre is not found.
+    """
+    # Convert genre to lowercase for case-insensitive matching
+    try:
+        genre = genre.lower().strip()
+        # Filter the dataframe to retain only rows with the specified genre
+        genre_df = hours_per_year[hours_per_year['Genre'].str.lower() == genre]
+
+        # Check if any rows are found for the genre
+        if not genre_df.empty:
+            # Find the year with the highest total playtime for the genre
+            max_playtime_year = genre_df.loc[genre_df['Playtime_Forever'].idxmax(), 'Release_Year']
+            return int(max_playtime_year)
+        else:
+            # Genre not found in the dataframe
+            return f"Couldn't find the {genre} genre. Please check your input."
     except Exception as e:
-        # If a KeyError occurs, return None
+        print("An error occurred while processing your request.")
         print(e)
-        return e
+        raise e
+    
+def max_player_time_per_genre(hours_per_year, player_maxtime_genre, genre):
+    genre = genre.lower().strip()
+    genre_df = hours_per_year[hours_per_year['Genre'].str.lower() == genre]
+    try:
+        if genre_df.empty:
+            return f"Couldn't find the {genre} genre. Please check your input." 
+        
+        genre_filter = player_maxtime_genre.groupby(player_maxtime_genre['Posted_Year'])['Playtime_Forever'].sum().reset_index()
+        maxtime_user = player_maxtime_genre.loc[player_maxtime_genre['Playtime_Forever'].idxmax()]['User_Id']
+        response = {
+            "User with most hours for " + genre: maxtime_user,
+            "Hours played:": [{"Year": year, "Hours": hours} for year, hours in zip(genre_filter['Posted_Year'], genre_filter['Playtime_Forever'])]
+        }
+
+        return response
+    
+    except Exception as e:
+        print("An error occurred while processing your request.")
+        print(e)
+
+        raise e
